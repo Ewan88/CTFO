e9d1ead155b53bc3b81cb1f5a1fa6bd80920210d
Collin Bull <vhiessumaniac@hotmail.com>
2019-04-13 14:50:48 +0100

    experimenting

--- ctfo_client/src/Components/MoodJournal.js
+++ ctfo_client/src/Components/MoodJournal.js
@@ -1,24 +1,59 @@
 import React, {Component} from 'react';
 import Calendar from 'react-calendar';
 
+
+
 class MoodJournal extends Component {
 
-  state = {
-    date: new Date(),
-  }
+  constructor(props) {
+        super(props);
 
-  onChange = date => this.setState({ date })
+        this.state = {
+            myEvents: []
+        };
+
+        mobiscroll.util.getJson('https://trial.mobiscroll.com/events/', (events) => {
+            this.setState({ myEvents: events});
+        }, 'jsonp');
+    }
 
 
-  render(){
-    return (
-      <div>
-        <h1>Mood Journal</h1>
-        <Calendar onChange={this.onChange}
-        value={this.state.date} />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <mobiscroll.Eventcalendar
+                theme="ios"
+                display="inline"
+                data={this.state.myEvents}
+                view={{
+                    calendar: { type: 'month' },
+                    eventList: { type: 'month', scrollable: true }
+                }}
+                data={this.state.myEvents}
+            />
+        );
+    }
+
+
+
+  // state = {
+  //   date: new Date(),
+  // }
+  //
+  // onChange = date => this.setState({ date })
+  //
+  //
+  //
+  // render(){
+  //   return (
+  //     <div>
+  //       <h1>Mood Journal</h1>
+  //       <Calendar onChange={this.onChange}
+  //       value={this.state.date}
+  //       onClickDay={this.onClickDay} />
+  //
+  //     </div>
+  //   )
+  // }
 }
 
 export default MoodJournal;
