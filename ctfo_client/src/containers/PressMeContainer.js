4aeeda4d23a7b5afb04904ba92ca39fc7a20611b
Hevski <heathercmalloch@gmail.com>
2019-04-13 15:25:30 +0100

    can displey emergency kittens

--- ctfo_client/src/containers/PressMeContainer.js
+++ ctfo_client/src/containers/PressMeContainer.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import QuoteMe from '../Components/QuoteMe';
+import EmergencyKittens from '../Components/EmergencyKittens';
 
 class PressMeContainer extends Component {
   render(){
     return (
+    <React.Fragment>
     <QuoteMe />
+    <EmergencyKittens />
+    </React.Fragment>
   )
   }
 
