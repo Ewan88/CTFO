8c2e7606ca7c4127211997092a737c404eef8ce4
Collin Bull <vhiessumaniac@hotmail.com>
2019-04-13 12:44:07 +0100

    dunno

--- ctfo_client/src/containers/MainContainer.js
+++ ctfo_client/src/containers/MainContainer.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 
-
 class MainContainer extends Component {
 
   render() {
     return (
       <div className="App">
         <h1>How are you feeling today?</h1>
-        <label htmlFor="customRange1">☺    </label>
+        <label htmlFor="customRange1">☹    </label>
         <input type="range" className="custom-range" id="customRange1" />
-        <label htmlFor="customRange1">    ☹</label>
+        <label htmlFor="customRange1">    ☺</label>
       </div>
     );
   }
