c59d7e81270b0fe99143c3bb8fc0b22c4d58a133
Collin Bull <vhiessumaniac@hotmail.com>
2019-04-14 12:05:36 +0100

    added event handler for slider

--- ctfo_client/src/Components/Home.js
+++ ctfo_client/src/Components/Home.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
+  constructor(){
+    super();
+    this.state = {
+      mood_id: 3
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({mood_id: event.target.value})
+  }
 
   render() {
     return (
       <div className="App">
         <h1>How are you feeling today?</h1>
         <label htmlFor="customRange1">☹    </label>
-        <input type="range" className="custom-range" id="customRange1" />
+          <input
+            type="range"
+            value={this.state.mood_id}
+            onChange={this.handleChange}
+            min="1"
+            max="5"
+            className="custom-range"
+            id="customRange1" />
         <label htmlFor="customRange1">    ☺</label>
       </div>
     );
