3ab80d79deedc9d6fce7e0de032f91360f25185a
Ewan88 <baileyewan@gmail.com>
2019-04-16 11:28:16 +0100

    commiting before adding new package

--- ctfo_client/src/Components/MoodJournal.js
+++ ctfo_client/src/Components/MoodJournal.js
@@ -8,35 +8,35 @@ class MoodJournal extends Component {
     super(props);
     this.state = {
       date: new Date(),
-      entries_holder: [],
       entries: []
     }
     this.onChange = this.onChange.bind(this);
   }
 
   componentDidMount(){
-    console.log(this.state.date);
     const request = new Request();
     request.get('/api/journals')
     .then(data => {
-      this.setState({entries_holder: data._embedded.journals})
+      this.setState({entries: data._embedded.journals})
     })
     .then(this.loadEntries())
   }
 
   loadEntries(){
     console.log('loading entries...');
+    console.log(this.state.entries);
     let a = [];
     let j = 0;
     if (this.state.entries) {
-      for (let i = 0; i < this.state.entries_holder.length; i++){
+      for (let i = 0; i < this.state.entries.length; i++){
         // console.log(this.state.entries[i].date);
         // console.log(this.state.entries[i].date instanceof Date);
-        a.push(this.state.entries_holder[i].date)
+        a.push(this.state.entries[i].date);
         console.log(a[j]);
         console.log(a[j].constructor.name);
         console.log(a[j] instanceof String);
         j++;
+        console.log(a);
       }
       return this.loadCalendar(a);
     } else {
@@ -45,12 +45,10 @@ class MoodJournal extends Component {
   }
 
   loadCalendar(a){
-    console.log(this.state.entries);
-    console.log(a);
     if (this.state.entries) {
       return (
         <Calendar onChange={this.onChange} view="month"
-        value={this.state.date} />
+        value={this.state.entries} />
       )
     } else {
       return
